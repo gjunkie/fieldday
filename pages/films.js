b860204577b965761a25b5a0dde6692b826943cd
Freddie Carthy <fcarthy@gmail.com>
2021-08-29 14:49:09 -0700

    initial mosaic view for films

--- pages/films.js
+++ pages/films.js
@@ -1,12 +1,63 @@
 import * as React from 'react';
 import { Header } from '../components/Header';
 
+import {
+  Image,
+  Main
+} from '../styles/films';
+
+const films = [
+  {
+    id: 1,
+    url: 'https://cataas.com/cat?width=300&height=500',
+  },
+  {
+    id: 2,
+    url: 'https://cataas.com/cat?width=300&height=500',
+  },
+  {
+    id: 3,
+    url: 'https://cataas.com/cat?width=300&height=450',
+  },
+  {
+    id: 4,
+    url: 'https://cataas.com/cat?width=300&height=500',
+  },
+  {
+    id: 5,
+    url: 'https://cataas.com/cat?width=300&height=450',
+  },
+  {
+    id: 6,
+    url: 'https://cataas.com/cat?width=300&height=500',
+  },
+  {
+    id: 7,
+    url: 'https://cataas.com/cat?width=300&height=475',
+  },
+  {
+    id: 8,
+    url: 'https://cataas.com/cat?width=300&height=500',
+  },
+  {
+    id: 9,
+    url: 'https://cataas.com/cat?width=300&height=450',
+  },
+  {
+    id: 10,
+    url: 'https://cataas.com/cat?width=300&height=500',
+  },
+];
+
 const Films = () => {
   return (
     <>
       <Header />
-      <main>
-      </main>
+      <Main>
+        {films.map((film) => (
+          <Image key={film.id} src={film.url} />
+        ))}
+      </Main>
     </>
   );
 };
---
+++ styles/films.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  line-height: 0;
+   
+  -webkit-column-count: 5;
+  -webkit-column-gap:   0px;
+  -moz-column-count:    5;
+  -moz-column-gap:      0px;
+  column-count:         5;
+  column-gap:           0px;
+`;
+
+export const Image = styled.img`
+  width: 100% !important;
+  height: auto !important;
+`;
