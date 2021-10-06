e1ebecf5a0e2cb837ce7ca357a8a910c573a308e
Freddie Carthy <fcarthy@gmail.com>
2021-10-05 22:08:34 -0700

    swap home page video with vimeo

--- pages/index.js
+++ pages/index.js
@@ -30,14 +30,12 @@ export default function Home() {
       <Header />
 
       <main className={styles.main}>
-        {/* <video ref={onVideo} className="video-js" onPlay={onPlay} onPause={onPlay} playsInline /> */}
         <h1>field day</h1>
-        {/* <div className={styles.videoWrapper}> */}
-        {/*   <video playsInline autoPlay muted loop poster="/images/home-poster.png" id="bgvid"> */}
-        {/*     {/* <source src="polina.webm" type="video/webm"> } */}
-        {/*     <source src="/videos/home-bg.mov" type="video/mp4" /> */}
-        {/*   </video> */}
-        {/* </div> */}
+        <div className={styles.videoWrapper}>
+          <video playsInline autoPlay muted loop poster="/images/home-poster.png" id="bgvid">
+            <source src="https://player.vimeo.com/external/624066986.hd.mp4?s=1cf6780146a1457147a74e2ac666dd0275043fe6&profile_id=174" type="video/mp4" />
+          </video>
+        </div>
       </main>
     </>
   )
