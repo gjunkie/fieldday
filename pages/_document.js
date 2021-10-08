05e7f0f5b400147de10f85abe9b5e77d01ebb77c
Freddie Carthy <fcarthy@gmail.com>
2021-10-07 21:07:21 -0700

    preload css

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,33 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  static async getInitialProps(page) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = page.renderPage;
+
+    try {
+      page.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(page);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
   render() {
     return (
       <Html lang="en">
