3e9d05701da0f613c73963b84db7e78ee7fc8bcd
Freddie Carthy <fcarthy@gmail.com>
2022-02-05 15:03:39 -0800

    test deploy

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,6 @@ export default function Home() {
               <img alt="Field Day Sound" src="images/logo.png" />
             </h1>
 
-
             <video playsInline autoPlay muted loop poster={posterUrl} id="bgvid">
               <source src="https://player.vimeo.com/external/624066986.hd.mp4?s=1cf6780146a1457147a74e2ac666dd0275043fe6&profile_id=174" type="video/mp4" />
             </video>
